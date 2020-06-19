644ee7c5b9f0b360fc2d2f58bdf2161f44fb3aa3
Galina Marakina <g.marakina.2gis@gmail.com>
2020-06-19 10:42:00 +0300

    Add check2

--- app/src/main/java/com/example/authentication/MainActivity.java
+++ app/src/main/java/com/example/authentication/MainActivity.java
@@ -20,20 +20,21 @@ import java.text.BreakIterator;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText userName;
+    private EditText userPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText userName = findViewById(R.id.edit_login);
-        EditText userPassword = findViewById(R.id.edit_password);
+        userName = findViewById(R.id.edit_login);
+        userPassword = findViewById(R.id.edit_password);
 
-        if (!userName.getText().toString().isEmpty() || userPassword.getText().toString().isEmpty()) {
-            Toast.makeText(MainActivity.this, R.string.toast1, Toast.LENGTH_LONG).show();
-        } else {
-            saveUser();
-            registrationUser();
-        }
+        Toast.makeText(MainActivity.this, R.string.toast1, Toast.LENGTH_LONG).show();
+
+        saveUser();
+        registrationUser();
     }
 
     private void saveUser() {
@@ -41,25 +42,29 @@ public class MainActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String filename = "login.txt";
-                // Получим входные байты из файла которых нужно прочесть.
-                FileInputStream fileInputStream = null;
-                try {
-                    fileInputStream = openFileInput(filename);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
+                if (!userName.getText().toString().isEmpty() || userPassword.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, R.string.toast1, Toast.LENGTH_LONG).show();
+                } else {
+                    String filename = "login.txt";
+                    // Получим входные байты из файла которых нужно прочесть.
+                    FileInputStream fileInputStream = null;
+                    try {
+                        fileInputStream = openFileInput(filename);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    // Декодируем байты в символы
+                    assert fileInputStream != null;
+                    InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+                    // Читаем данные из потока ввода, буферизуя символы так, чтобы обеспечить эффективную запись отдельных символов.
+                    BufferedReader reader = new BufferedReader(inputStreamReader);
+                    try {
+                        reader.readLine();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(MainActivity.this, R.string.toast2, Toast.LENGTH_LONG).show();
                 }
-                // Декодируем байты в символы
-                assert fileInputStream != null;
-                InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
-                // Читаем данные из потока ввода, буферизуя символы так, чтобы обеспечить эффективную запись отдельных символов.
-                BufferedReader reader = new BufferedReader(inputStreamReader);
-                try {
-                    reader.readLine();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                Toast.makeText(MainActivity.this, R.string.toast2, Toast.LENGTH_LONG).show();
             }
         });
     }
@@ -69,32 +74,35 @@ public class MainActivity extends AppCompatActivity {
         registration.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                // Создадим файл и откроем поток для записи данных
-                FileOutputStream fileOutputStream = null;
-                try {
-                    fileOutputStream = openFileOutput("file_login", MODE_PRIVATE);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
+                if (!userName.getText().toString().isEmpty() || !userPassword.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, R.string.toast1, Toast.LENGTH_LONG).show();
+                } else {
+                    // Создадим файл и откроем поток для записи данных
+                    FileOutputStream fileOutputStream = null;
+                    try {
+                        fileOutputStream = openFileOutput("file_login", MODE_PRIVATE);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    // Обеспечим переход символьных потока данных к байтовым потокам.
+                    assert fileOutputStream != null;
+                    OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+                    // Запишем текст в поток вывода данных, буферизуя символы так, чтобы обеспечить эффективную запись отдельных символов.
+                    BufferedWriter bw = new BufferedWriter(outputStreamWriter);
+                    // запись данных
+                    try {
+                        bw.write("Содержимое файла");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    // закрытие потока
+                    try {
+                        bw.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(MainActivity.this, R.string.toast3, Toast.LENGTH_LONG).show();
                 }
-                // Обеспечим переход символьных потока данных к байтовым потокам.
-                assert fileOutputStream != null;
-                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
-                // Запишем текст в поток вывода данных, буферизуя символы так, чтобы обеспечить эффективную запись отдельных символов.
-                BufferedWriter bw = new BufferedWriter(outputStreamWriter);
-                // запись данных
-                try {
-                    bw.write("Содержимое файла");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                // закрытие потока
-                try {
-                    bw.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                Toast.makeText(MainActivity.this, R.string.toast3, Toast.LENGTH_LONG).show();
             }
         });
     }
