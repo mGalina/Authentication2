dd601d42b5cfe442caed998c3cc2ae947008fc19
Galina Marakina <g.marakina.2gis@gmail.com>
2020-06-25 13:48:44 +0300

    No commit

--- app/src/main/java/com/example/authentication/MainActivity.java
+++ app/src/main/java/com/example/authentication/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private SharedPreferences pref;
     private static final String save_key = "save_key";
     private static final String FILENAME = "login.txt";
+    private static final String EXTERNAL_KEY = "EXTERNAL_KEY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,26 +48,20 @@ public class MainActivity extends AppCompatActivity {
         externalStorage = findViewById(R.id.checkBox_external_storage);
 
         pref = getSharedPreferences("myLogin", MODE_PRIVATE);
+
+        setCheckBoxListener();
+
+        pref = getSharedPreferences("myLogin", MODE_PRIVATE);
+        externalStorage.setChecked(pref.getBoolean(EXTERNAL_KEY, false));
     }
 
-    public void onClickCheck(View view) {
+    private void setCheckBoxListener() {
         externalStorage.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    Toast.makeText(MainActivity.this, R.string.toast6, Toast.LENGTH_LONG).show();
-                    savePrefsData();
-                } else {
-                    registrationUser();
-                }
+                pref.edit().putBoolean(EXTERNAL_KEY, isChecked).apply();
             }
         });
-    }
-
-    private void savePrefsData() {
-        SharedPreferences.Editor edit = pref.edit();
-        edit.putBoolean(save_key, Boolean.parseBoolean(externalStorage.getText().toString()));
-        edit.apply();
     }
 
     private void saveUser() {
@@ -104,27 +99,34 @@ public class MainActivity extends AppCompatActivity {
 
     private void registrationUser() {
         Button registration = findViewById(R.id.btn_registration);
-        registration.setOnClickListener(new View.OnClickListener() {
+        registration.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 String userNameText = userName.getText().toString();
                 String passwordText = userPassword.getText().toString();
 
-                if (userNameText.isEmpty() || passwordText.isEmpty()) {
-                    Toast.makeText(MainActivity.this, R.string.toast1, Toast.LENGTH_LONG).show();
-                    return;
-                }
+                if (externalStorage != null) {
 
-                try (FileOutputStream fileOutputStream = openFileOutput(FILENAME, MODE_PRIVATE);
-                     OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
-                     BufferedWriter bw = new BufferedWriter(outputStreamWriter)) {
-                    bw.write(userNameText);
-                    bw.write("\n");
-                    bw.write(passwordText);
 
-                    Toast.makeText(MainActivity.this, R.string.toast3, Toast.LENGTH_LONG).show();
-                } catch (IOException e) {
-                    e.printStackTrace();
+                } else {
+
+                    if (userNameText.isEmpty() || passwordText.isEmpty()) {
+                        Toast.makeText(MainActivity.this, R.string.toast1, Toast.LENGTH_LONG).show();
+                        return;
+                    }
+
+                    try (FileOutputStream fileOutputStream = openFileOutput(FILENAME, MODE_PRIVATE);
+                         OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+                         BufferedWriter bw = new BufferedWriter(outputStreamWriter)) {
+                        bw.write(userNameText);
+                        bw.write("\n");
+                        bw.write(passwordText);
+
+                        Toast.makeText(MainActivity.this, R.string.toast3, Toast.LENGTH_LONG).show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
